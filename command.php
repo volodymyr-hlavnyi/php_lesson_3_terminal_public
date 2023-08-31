a88b743b46ce9a33f9cc3257f00c021580ef4199
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-09-01 00:15:08 +0200

    add check if without argv

--- command.php
+++ command.php
@@ -8,19 +8,23 @@
 
 echo "HW 3. Перша консольна програма" . PHP_EOL;
 echo "------------------------------" . PHP_EOL;
-echo "part #1".PHP_EOL;
-echo "=======".PHP_EOL;
+echo "part #1" . PHP_EOL;
+echo "=======" . PHP_EOL;
 echo "What is your name ?" . PHP_EOL;
 $name_user = fgets(STDIN);
 echo "Hi, " . trim($name_user) . ". We are welcome you! " . PHP_EOL;
-echo "part #2".PHP_EOL;
-echo "=======".PHP_EOL;
+echo "part #2" . PHP_EOL;
+echo "=======" . PHP_EOL;
 $sum_numbers = 0;
 $average = 0;
-foreach($argv as $value){
+foreach ($argv as $value) {
     $sum_numbers = $sum_numbers + intval($value);
 }
-$average = $sum_numbers / (count($argv)-1);
+if (count($argv) > 2) {
+    $average = $sum_numbers / (count($argv) - 1);
+} else {
+    $average = $sum_numbers;
+}
 echo "Sum = $sum_numbers" . PHP_EOL;
 echo "Avg = $average" . PHP_EOL;
 
