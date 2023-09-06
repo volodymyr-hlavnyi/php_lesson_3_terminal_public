9d35bc281e5c10cbe0da912e3a617e8872880952
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-09-06 16:32:53 +0200

    HW 5

---
+++ homework5.php
@@ -0,0 +1,35 @@
+<?php
+//- 1 - green
+//- 2 - red
+//- 3 - blue
+//- 4 - brown
+//- 5 - violet
+//- 6 - black
+echo "HW 5. Вивід в залежності від умови" . PHP_EOL;
+
+$value = 1;
+
+echo "value = $value".PHP_EOL;
+
+switch ($value) {
+    case 1:
+        echo "green" . PHP_EOL;
+        break;
+    case 2:
+        echo "red" . PHP_EOL;
+        break;
+    case 3:
+        echo "blue" . PHP_EOL;
+        break;
+    case 4:
+        echo "brown" . PHP_EOL;
+        break;
+    case 5:
+        echo "violet" . PHP_EOL;
+        break;
+    case 6:
+        echo "black" . PHP_EOL;
+        break;
+    default:
+        echo "white".PHP_EOL;
+}
